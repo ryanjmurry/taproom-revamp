85bfede87f4ced362d2546dbfcc4345a970c79e4
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 12:04:50 -0700

    add card group around tap components that are returned

--- src/components/TapList.js
+++ src/components/TapList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Tap from './Tap';
+import { Card } from 'semantic-ui-react';
 
 const TapList = (props) => {
   return (
     <div>
+      <Card.Group centered>
       {props.tapList.map((tap) =>
         <Tap 
           tap={tap}
           key={tap.id}
         />
       )}
+      </Card.Group>
     </div>
   );
 }
