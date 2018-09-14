f810e856b8c3a98a008c94d52bce63c0b46a92e2
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 15:04:31 -0700

    create component, add prop types, toggle site visibility if user selects they are over the age of 21

---
+++ src/components/Splash.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
+
+const Splash = (props) => {
+  return (
+    <div>
+      <h1>Just Tap It</h1>
+      <img src={require('./../assets/just-tap-it.png')} />
+      Are you over 21 years of age?
+      <Button onClick={props.onShowSiteContent}>
+        Yes
+      </Button>
+      <Button>
+        No
+      </Button>
+    </div>
+  );
+}
+
+Splash.propTypes = {
+  onShowSiteContent: PropTypes.func.isRequired
+}
+
+export default Splash;
