389df415bfec5a5699cb6f5bac6e87aaa40cbf56
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 08:42:39 -0700

    create constructor and add state. create method to add new tap to stateful masterlist and pass props into both taplist and new tap form

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import NewKegForm from './NewKegForm';
+import NewTapForm from './NewTapForm';
 import TapList from './TapList';
 import Nav from './Nav';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTapList: []
+    }
+    this.handleAddingNewTap = this.handleAddingNewTap.bind(this);
+  }
+
+  handleAddingNewTap(newTap) {
+    let newMasterTapList = this.state.masterTapList.slice();
+    newMasterTapList.push(newTap);
+    this.setState({
+      masterTapList: newMasterTapList
+    })
+  }
+
   render() {
     return (
       <div >
         <Nav />
         <Switch>
-          <Route exact path='/' component={TapList} />
-          <Route path='/newkeg' component={NewKegForm} />
+          <Route exact path='/' render={()=><TapList tapList={this.state.masterTapList} />} />
+          <Route path='/newtap' render={()=><NewTapForm onNewTapCreation={this.handleAddingNewTap} />} />
         </Switch>
 
       </div>
