d96821d8264b652b45859bfa93e8f32d7d1407ed
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 08:16:10 -0700

    import nav component and add component to be shown on all pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NewKegForm from './NewKegForm';
 import TapList from './TapList';
+import Nav from './Nav';
 
 class App extends React.Component {
   render() {
     return (
       <div >
+        <Nav />
         <Switch>
           <Route exact path='/' component={TapList} />
           <Route path='/newkeg' component={NewKegForm} />
