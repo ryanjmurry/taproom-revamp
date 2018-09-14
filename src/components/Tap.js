3c7bf07b6fc106595ecf9bff46e2f8905daafc8d
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 14:29:35 -0700

    refactor to become stateful component to handle modal open and close on edit form submit. pass callback to close modal to edit form

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Card, Progress, Button, Modal, Header, Icon } from 'semantic-ui-react';
 import EditTap from './EditTap';
 
-const Tap = (props) => {
- const { name, brewery, style, abv, pintPrice, growlerPrice, pintsRemaining, description, id } = props.tap;
+class Tap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    }
+  }
 
- const handlePintSale = () => {
-   props.onPintSale(id);
- }
+  handleModalOpen = () => {
+    this.setState({
+      modalOpen: true
+    })
+  }
 
- const handleGrowlerSale = () => {
-   props.onGrowlerSale(id)
- }
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false
+    })
+  }
 
- const handleEditTap = () => {
+  handlePintSale = () => {
+    this.props.onPintSale(this.props.tap.id);
+  }
+ 
+  handleGrowlerSale = () => {
+    this.props.onGrowlerSale(this.props.tap.id)
+  }
+ 
+  handleDeleteTap = () => {
+   this.props.onTapRemoval(this.props.tap.id);
+  }
 
- }
-
- const handleDeleteTap = () => {
-  props.onTapRemoval(id);
- }
-
-  return (
-    <div>
-      <Card>
+  render() {
+    const { name, brewery, style, abv, pintPrice, growlerPrice, pintsRemaining, description} = this.props.tap;
+    return (
+      <div>
+        <Card>
         <Card.Content>
           <Card.Header><span>{name}</span> by <span>{brewery}</span></Card.Header>
         </Card.Content>
@@ -33,31 +48,30 @@ const Tap = (props) => {
         </Card.Content>
         <Card.Content extra>
           <Progress progress='value' value={pintsRemaining} total={124} label='pints remaining' indicating/>
-          <Button onClick={handlePintSale}>Sell Pint</Button>
-          <Button onClick={handleGrowlerSale}>Sell Growler</Button>
-          <Button onClick={handleDeleteTap}>Delete</Button>
-          <Modal trigger={<Button>Edit</Button>} basic size='small'>
-            <Header icon='' content='' />
+          <Button onClick={this.handlePintSale}>Sell Pint</Button>
+          <Button onClick={this.handleGrowlerSale}>Sell Growler</Button>
+          <Button onClick={this.handleDeleteTap}>Delete</Button>
+          <Modal 
+            trigger={<Button onClick={this.handleModalOpen}>Edit</Button>}
+            open={this.state.modalOpen}
+            onClose={this.handleModalClose}
+            basic 
+            size='small'>
             <Modal.Content>
-            <EditTap 
-              tap={props.tap}
-              onTapUpdate={props.onTapUpdate}
-            />
+              <EditTap 
+                tap={this.props.tap}
+                onTapUpdate={this.props.onTapUpdate}
+                onModalClose={this.handleModalClose}
+              />
             </Modal.Content>
-            <Modal.Actions>
-              <Button basic color='red' inverted>
-                <Icon name='remove' /> Cancel
-              </Button>
-              <Button color='green' inverted>
-                <Icon name='checkmark' /> Submit
-              </Button>
-            </Modal.Actions>
           </Modal>
         </Card.Content>
       </Card>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
 
 Tap.propTypes = {
   tap: PropTypes.object.isRequired,
@@ -69,4 +83,17 @@ Tap.propTypes = {
 
 export default Tap;
 
-{/*  */}
+{/* <Header icon='' content='' />
+
+<EditTap 
+  tap={this.props.tap}
+  onTapUpdate={this.props.onTapUpdate}
+/>
+<Modal.Actions>
+  <Button basic color='red' inverted>
+    <Icon name='remove' /> Cancel
+  </Button>
+  <Button color='green' inverted>
+    <Icon name='checkmark' /> Submit
+  </Button>
+</Modal.Actions> */}
