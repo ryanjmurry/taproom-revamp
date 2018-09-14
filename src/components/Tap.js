d7a754468960c2eff4609770dfe412fe1d271445
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 11:38:27 -0700

    destructure props and add proptypes

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Tap = () => {
+const Tap = (props) => {
+ const { name, brewery, style, abv, pintPrice, growlerPrice, xlGrowlerPrice, description } = props.tap;
   return (
     <div>
       
     </div>
   );
+}
+
+Tap.propTypes = {
+  tap: PropTypes.object
 }
 
 export default Tap;
