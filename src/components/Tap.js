3236b56c4ba9169e63ef5103a95d1cd56b415607
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 12:05:46 -0700

    add basic styling and display all tap data

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, Progress } from 'semantic-ui-react';
 
 const Tap = (props) => {
- const { name, brewery, style, abv, pintPrice, growlerPrice, xlGrowlerPrice, description } = props.tap;
+ const { name, brewery, style, abv, pintPrice, growlerPrice, pintsRemaining, description } = props.tap;
   return (
     <div>
-      
+      <Card>
+        <Card.Content>
+          <Card.Header><span>{name}</span> by <span>{brewery}</span></Card.Header>
+        </Card.Content>
+        <Card.Content>
+          {style}, {abv}%, ${pintPrice}, ${growlerPrice}, {description}
+        </Card.Content>
+        {/* <Card.Content extra>
+          <Progress percent={pintsRemaining} indicating />
+        </Card.Content> */}
+      </Card>
     </div>
   );
 }
