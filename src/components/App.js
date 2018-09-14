0d10607b1ee6b8f881054138a878ce6a13e20f57
Ryan Murry <ryanjmurry@gmail.com>
2018-09-14 08:13:48 -0700

    import new keg form and tap list and add routing to each

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import NewKegForm from './NewKegForm';
+import TapList from './TapList';
 
 class App extends React.Component {
   render() {
     return (
       <div >
-        Testing
+        <Switch>
+          <Route exact path='/' component={TapList} />
+          <Route path='/newkeg' component={NewKegForm} />
+        </Switch>
+
       </div>
     );
   }
